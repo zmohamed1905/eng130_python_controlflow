542993b5e4ca2b7c7c8d7808ca49ab8588eb7d3e
zmohamed1905 <zakariya.m1905@gmail.com>
2022-10-12 14:19:53 +0100

    new repo control_flow with code

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Python Control Flow
 # if elif else
-### loops 
+### Loops - for loop - while loop
+#### Python keywords break - continue etc.
+# if it's cold:
+    take jacket
+# if its raining:
+    rain coat
+# if it's sunny: boolean value true- next line if false goes to else or elif
+    let's go to the beach
+# else
---
+++ control_flow.py
@@ -0,0 +1,32 @@
+# pseudo coding
+
+
+# weather = "time"
+#
+# # if it's cold:
+# if weather == "cold": # True or False
+#     print("wear a jacket")
+# #     take jacket
+# elif weather == "sunny":
+#     print("let's go to the beach")
+#
+# # if its raining:
+# else:
+#     print("no match better luck next time")
+#     rain coat
+# if it's sunny: boolean value true- next line if false goes to else or elif
+#     let's go to the beach
+# else
+
+# age restriction for movie tickets
+# create a condition for 18 & above
+# 16 & above
+# universal
+# pg
+# 12a
+# 15 & avoe
+# if nothing matches inform the user to enter their age again
+# user must not be allowed to enter age over 117 years
+
+print("Hello!, Welcome to Sparta Cinema")
+print("To watch the movie,")
